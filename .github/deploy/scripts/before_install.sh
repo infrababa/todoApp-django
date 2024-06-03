e3572bfcdd755c0a3d5fd8c3f10132128be4755c
abudev22 <sadiqoffial99@gmail.com>
2024-06-03 13:37:52 +0000

    updated main.yml

--- .github/deploy/scripts/before_install.sh
+++ .github/deploy/scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker pull ${{ secrets.DOCKER_HUB_USERNAME }}/devops:latest
+docker pull ${{ secrets.DOCKER_HUB_USERNAME }}/todo-api:latest
 docker stop my-website || true
 docker rm my-website || true
--- .github/deploy/scripts/start_server.sh
+++ .github/deploy/scripts/start_server.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name my-website -p 8000:8000 ${{ secrets.DOCKER_HUB_USERNAME }}/devops:latest
+docker run -d --name my-website -p 8000:8000 ${{ secrets.DOCKER_HUB_USERNAME }}/todo-api:latest
